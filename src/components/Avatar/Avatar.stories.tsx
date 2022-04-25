ba23ffe5caeb6b725e1f2f2809b80f1473e0f8a9
Matthew Gleed <matthew.gleed@28stone.com>
2022-04-25 14:10:01 +0300

    fix issue with avatar loading prop

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -25,12 +25,12 @@ Sizes.args = {
   src: 'https://avatars2.githubusercontent.com/u/132554',
 };
 
-export const Initials = () => (
+export const Initials = args => (
   <div>
-    <Avatar username="Tom Coleman" />
-    <Avatar username="Dominic Nguyen" />
-    <Avatar username="Kyle Suss" />
-    <Avatar username="Michael Shilman" />
+    <Avatar {...args} username="Tom Coleman" />
+    <Avatar {...args} username="Dominic Nguyen" />
+    <Avatar {...args} username="Kyle Suss" />
+    <Avatar {...args} username="Michael Shilman" />
   </div>
 );
 
@@ -46,10 +46,10 @@ Loading.args = {
   loading: true,
 };
 
-export const Large = () => (
+export const Large = args => (
   <div>
-    <Avatar loading size="large" />
-    <Avatar size="large" username="Tom Coleman" />
-    <Avatar size="large" username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
+    <Avatar {...args} loading size="large" />
+    <Avatar {...args} size="large" username="Tom Coleman" />
+    <Avatar {...args} size="large" username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
   </div>
 );
